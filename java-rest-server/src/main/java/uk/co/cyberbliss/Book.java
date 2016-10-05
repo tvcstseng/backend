45fb7415e22afbc550417e3f6b6c6f6d120223c5
Timothy Tseng <tvcstseng@gmail.com>
2016-10-05 20:04:41 +0200

    some formatting

--- java-rest-server/src/main/java/uk/co/cyberbliss/Book.java
+++ java-rest-server/src/main/java/uk/co/cyberbliss/Book.java
@@ -2,50 +2,51 @@ package uk.co.cyberbliss;
 
 import org.hibernate.validator.constraints.NotBlank;
 import org.springframework.hateoas.ResourceSupport;
-public class Book extends ResourceSupport{
 
-    private String isbn;
+public class Book extends ResourceSupport {
 
-    @NotBlank
-    private String title;
+	private String isbn;
 
-    private String author;
+	@NotBlank
+	private String title;
 
-    public Book() {
-    }
+	private String author;
 
-    public Book(String isbn, String title, String author) {
-        this.isbn = isbn;
-        this.title = title;
-        this.author =author;
-    }
+	public Book() {
+	}
 
-    public String getIsbn() {
-        return isbn;
-    }
+	public Book(String isbn, String title, String author) {
+		this.isbn = isbn;
+		this.title = title;
+		this.author = author;
+	}
 
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
-    }
+	public String getIsbn() {
+		return isbn;
+	}
 
-    public String getTitle() {
-        return title;
-    }
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
+	public String getTitle() {
+		return title;
+	}
 
-    public String getAuthor() {
-        return author;
-    }
+	public void setTitle(String title) {
+		this.title = title;
+	}
 
-    public void setAuthor(String author) {
-        this.author = author;
-    }
+	public String getAuthor() {
+		return author;
+	}
 
-    @Override
-    public String toString() {
-        return "Book{" + "isbn='" + isbn + '\'' + ", title='" + title + '\'' + '}';
-    }
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	@Override
+	public String toString() {
+		return "Book{" + "isbn='" + isbn + '\'' + ", title='" + title + '\'' + '}';
+	}
 }
